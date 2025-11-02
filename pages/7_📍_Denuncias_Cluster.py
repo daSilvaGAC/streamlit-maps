fb1a19e25c65908857cfdb87de890540ba3cb1c7
Guilherme Antunes <guilhermeantunescs@gmail.com>
2025-11-02 20:19:46 -0300

    fix: center map on Maringá and adjust zoom level

--- pages/7_📍_Denuncias_Cluster.py
+++ pages/7_📍_Denuncias_Cluster.py
@@ -16,12 +16,9 @@ st.title("Clusters de Denúncias")
 
 with st.expander("See source code"):
     with st.echo():
-
-        m = leafmap.Map(center=[-23.4205, -51.9333], zoom=4)
+        
+        m = leafmap.Map(center=[-23.415367,-51.931343], zoom=12)
         denuncias = "https://raw.githubusercontent.com/daSilvaGAC/streamlit-maps/refs/heads/main/mga_denuncias_20-23.csv"
-        # Centraliza o mapa no retângulo de Maringá e ajusta o zoom
-        bounds = [[-23.466711, -52.006187], [-23.366526, -51.867485]]
-        m.zoom_to_bounds(bounds)
         ## regions = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_regions.geojson"
 
         ## m.add_geojson(regions, layer_name="US Regions")
